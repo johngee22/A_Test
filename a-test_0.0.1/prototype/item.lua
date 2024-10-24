3651c2a3d6c50acbe227ce54f0ff54827d6124ce
johngee22 <johngee12323@gmail.com>
2024-10-24 07:03:59 -0700

    stuff

---
+++ a-test_0.0.1/prototype/item.lua
@@ -0,0 +1,13 @@
+--item.lua
+
+local iron2 = {
+	name = "iron2",
+	stack_size = 100,
+	type = "item",
+	icon = "__a-test__/graphics/icons/item-name.png",
+	icon_size = 64,
+}
+
+data:extend({
+	iron2
+})
