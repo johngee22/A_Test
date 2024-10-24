b22732198818d368b4faa813a0e40720e94e6f3a
johngee22 <johngee12323@gmail.com>
2024-10-24 05:57:43 -0700

    free

--- a-test_0.0.1/prototypes/recipe.lua
+++ a-test_0.0.1/prototypes/recipe.lua
@@ -4,7 +4,7 @@ local iron2 = {
 	name = "iron2",
 	type = "recipe",
 	ingredients = {
-	{type = "item", name = "iron-plate", amount = 5},
+	--{type = "item", name = "iron-plate", amount = 5},
 	},
 	results = {
 	{type = "item", name = "iron-stick", amount = 1}
